f6fc5e65a85ce1e01ac466569e0f228695e6f329
Juan Alves Pedreira <juan185067@gmail.com>
2023-09-05 00:09:30 -0300

    added modal interactivity

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 'use strict'
 
-console.log('my portfolio')
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.works__img')) {
+    const modal = document.querySelector('.modal')
+    const modalImage = document.querySelector('.modal__img')
+    const imageSrc = e.target.src
+
+    modalImage.src = imageSrc
+    modal.classList.add('modal--active')
+  }
+
+  if (e.target.matches('.modal__close')) {
+    const modal = document.querySelector('.modal')
+    modal.classList.remove('modal--active')
+  }
+
+  if (e.target.matches('.modal')) {
+    const modal = document.querySelector('.modal')
+    modal.classList.remove('modal--active')
+  }
+})
