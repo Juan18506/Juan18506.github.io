5a270d5856e6fcd83e8a0a1c09e915f02835d044
Juan Alves Pedreira <juan185067@gmail.com>
2023-12-13 20:29:38 -0300

    new works

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 matchMedia('(prefers-color-scheme: dark)').matches
-? document.documentElement.classList.add('dark')
-: document.documentElement.classList.add('light');
+  ? document.documentElement.classList.add('dark')
+  : document.documentElement.classList.add('light');
 
 observeWorksImages();
 
